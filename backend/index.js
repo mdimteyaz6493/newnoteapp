8f5a7ce27d9916e49d65d2ba1a146a0c36d9e21d
mdimteyaz6493 <mdimteyaz6493@gmail.com>
2025-03-13 13:00:36 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected to Compass"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
 // Routes
